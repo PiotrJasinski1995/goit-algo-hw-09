6b2247383a7847f788225f7015b7d4e3bdcbb506
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2024-06-30 19:29:46 +0200

    feat: implement time measuring and delete unnecessary comments

--- coin_change.py
+++ coin_change.py
@@ -1,17 +1,27 @@
 import sys
 from collections import Counter
+from timeit import default_timer as timer
 
 
 COINS = [50, 25, 10, 5, 2, 1]
 
+# defining decorator for time measuring
+def measure_execution_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = timer()
+        result = func(*args, **kwargs)
+        end_time = timer()
+        execution_time = end_time - start_time
+        print(f"Function {func.__name__} took {execution_time:.4f} seconds to execute")
+        return result
+    return wrapper
 
+
+@measure_execution_time
 def find_coins_greedy(money_given):
     money_sum = money_given
     coin_possible = True
     coin_dict = {}
-    
-    # for coin in COINS:
-    #     coin_dict[coin] = 0
 
     while coin_possible:
         if money_sum == 0:
@@ -46,28 +56,13 @@ def min_dict(list_of_dicts):
     return list_of_dicts[min_dict_index]
         
 
-
+@measure_execution_time
 def find_min_coins(V):
-     
-    # table[i] will be storing the minimum 
-    # number of coins required for i value. 
-    # So table[V] will have result
-    # table = [{} for _ in range(V + 1)]
-    # [[0 for w in range(W + 1)] for i in range(n + 1)]
     coins = sorted(COINS)
     coins.insert(0, 0)
     table = [[{} for _ in range(len(coins))] for _ in range(V + 1)]
     min_table = [{} for _ in range(V + 1)]
- 
-    # # Base case (If given value V is 0)
-    # table[0] = 0
- 
-    # # Initialize all table values as Infinite
-    # for i in range(1, V + 1):
-    #     table[i] = sys.maxsize
- 
-    # Compute minimum coins required 
-    # for all values from 1 to V
+
     for i in range(1, V + 1):
         if i == V:
             ala = 'ma kota'
@@ -82,47 +77,13 @@ def find_min_coins(V):
                 if (sub_res != {}):
                      table[i][j].update(Counter(table[i][j]) + Counter(sub_res))
         
-        min_table[i] = min_dict(table[i])
-
-        
-     
-    # if table[V] == sys.maxsize:
-    #     return -1
-       
+        min_table[i] = min_dict(table[i])    
     return dict(sorted(min_table[V].items()))
-
-
-# def dpChangeMoney(total, units, stored, min_ix=0):
-#     if total < 0:
-#         return []
-
-#     if total == 0:
-#         return [{}]
-
-#     if min_ix == len(units):
-#         return []
-
-#     key = (total, min_ix)
-#     if key in stored:
-#         return stored[key]
-
-#     sol_list = []
-#     u = units[min_ix]
-#     for c in range(total // u + 1):
-#         sols = dpChangeMoney(total - c*u, units, stored, min_ix + 1)
-#         for sol in sols:
-#             if c > 0:
-#                 sol2 = sol.copy()
-#                 sol2[u] = c
-#             else:
-#                 sol2 = sol
-#             sol_list.append(sol2)
-
-#     stored[key] = sol_list
-#     return sol_list
 
 
 print(find_coins_greedy(113))
 print(find_coins_greedy(200))
+print(find_coins_greedy(345321))
 print(find_min_coins(113))
 print(find_min_coins(200))
+print(find_min_coins(345321))
